8b3e197a9a4d80ba1643b01616b324cfe55d25c2
cmmisura14 <cmmisura14@gmail.com>
2015-04-29 14:45:59 -0500

    Second Commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var apikey = '10d0a85137d94904c75e456dcdf91468227c09ca'; // Put your API key her
 var j;
 var k;
 var i;
-var count;
+var count = 0;
 var gameArray = [];
 
 
@@ -31,24 +31,27 @@ function search(query){
 function searchCallback(results) {
     console.log(results);
     gameArray = results.slice(0);
+    console.log(gameArray);
     displayContent(gameArray, 0);
-    buttonCreater();
+    
 }
 
 function buttonCreater(){
-	$(".buttonContainer").append("<div class='btn left-button'>Left</div>");
+	$(".buttonContainer").append("<div class='row'><div class='left-button col-md-2'>Left</div>");
 	for(j = 0; j < 8; j++){
-		$(".buttonContainer").append("<div class='circle" + j +" circles'>" + j + "</div>");
+		$(".buttonContainer").append("<div class='deselected circle" + j +" circles col-md-1'>" + j + "</div>");
 	}
-	$(".buttonContainer").append("<div class='btn right-button'>Right</div>");
+	$(".buttonContainer").append("<div class='right-button col-md-2'>Right</div></div>");
 }
 
 function displayContent(results, count) {
 	$(".content").empty();
+	$(".footer").empty();
 
+		var consoles = '';
 		for(var k = 0; k < results[count].platforms.length; k++){
-			var platforms = '';
-			platforms += results[count].platforms[k].name + "<br>";
+			
+			consoles += results[count].platforms[k].name + "<br>";
 		}
 
 		var image = results[count].image.medium_url;
@@ -61,10 +64,16 @@ function displayContent(results, count) {
 				} else {
 					var description = "No Info Available";
 				}
-		$(".content").hide().append("<div class='col-md-8 well bg-info results group" + gameId + "'><img class='hidden-sm hidden-xs' src='" + image +"'/><br><button class='btn-default btn-sm minify'>Remove Item</button><button class='btn-success btn-sm showInfo'>Show Info</button></div>");
-		$(".group"+gameId).append("<div class='appendPlatform'><p class='lead'>" + name + "</p><p>" + description +"</p><p>Platform: " + platforms + "</p><a href='" + gameLink + "'>More info...</a><div class='buttonContainer row'></div></div><button class='btn-sm btn-warning removeInfo'>Remove Info</button>");
+		$(".content").hide().append("<div class='col-md-12 well bg-info results group" + gameId + "'><img class='hidden-sm hidden-xs' src='" + image +"'/><br><button class='btn-default btn-sm minify'>Remove Item</button><button class='btn-success btn-sm showInfo'>Show Info</button></div>");
+		$(".group"+gameId).append("<div class='appendPlatform'><p class='lead'>" + name + "</p><p>" + description +"</p><p>Platform: " + consoles + "</p><a href='" + gameLink + "'>More info...</a></div><button class='btn-sm btn-warning removeInfo'>Remove Info</button>");
+		$(".footer").append("<div class='buttonContainer'></div>");
+
+		$(".circle"+count).removeClass(".deselected");
+		$(".circle"+count).addClass(".selected");
+
 		$(".content").fadeIn("slow");
 		$(".results").css({height: '700px'});
+		buttonCreater();
 }
 
 
@@ -89,7 +98,7 @@ $(document).ready(function() {
 		$(".results").css({height: '600px'});
 		$(".results").fadeIn("slow");
 		$(".results").children().children().fadeIn("fast");
-		});
+	});
 
 	$(".content").on("click", ".showInfo", function(){
 		console.log("Show Info");
@@ -102,6 +111,27 @@ $(document).ready(function() {
 		$(this).fadeOut();
 		$(this).siblings(".appendPlatform").fadeOut();
 		//$(this).parent().css({height: 'auto'});
+	});
+
+	$(".footer").on("click", ".left-button", function() {
+		count--;
+		console.log(count);
+		if (count < 0){
+			count = 7;
+		}
+		$(".circle" + (count+1) ).removeClass(".selected");
+		$(".circle" + (count+1) ).addClass(".deselected");
+		displayContent(gameArray, count);
+	});
+
+	$(".footer").on("click", ".right-button", function() {
+		count++;
+		if (count > 7){
+			count = 0;
+		}
+		$(".circle" + (count-1) ).removeClass(".selected");
+		$(".circle" + (count-1) ).addClass(".deselected");
+		displayContent(gameArray, count);
 	});
 
 	
